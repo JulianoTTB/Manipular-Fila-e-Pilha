6c443eb13a8732c205dbe36b4517ddcb01cea76b
GeandroMarques <marquess.2608@gmail.com>
2025-09-07 12:12:38 -0300

    Alteração no metodo merge utilizando ponteiros e criação do metodo merge utilizando nós

--- src/Merge.java
+++ src/Merge.java
@@ -1,49 +1,63 @@
 public class Merge {
      private int ponteiro1;
      private int ponteiro2;
-     private int ponteiro3;
 
     public Merge() {
     }
 
-    public int [] merge(Fila f1, Fila f2) {
-        int tamanhoTotal = f1.getTamanho() + f2.getTamanho();
-        int [] c = new int [tamanhoTotal];
+    public Fila mergePonteiro(Fila f1, Fila f2) {
+        Fila merge = new Fila();
 
         ponteiro1 = 0;
         ponteiro2 = 0;
-        ponteiro3 = 0;
 
         while(ponteiro1 < f1.getTamanho() && ponteiro2 < f2.getTamanho()) {
-            if(f1.getFila[ponteiro1] < f2.getFila[ponteiro2]) {
-                c[ponteiro3] = f1.getFila[ponteiro1];
-                ponteiro3 += 1;
+            if(f1.getInfo(ponteiro1) < f2.getInfo(ponteiro2)) {
+                merge.inserir(f1.getInfo(ponteiro1));
                 ponteiro1 += 1;
-                c[ponteiro3] = f2.getFila[ponteiro2];
-                ponteiro3 += 1;
-                ponteiro2 += 1;
             } else {
-                c[ponteiro3] = f2.getFila[ponteiro2];
-                ponteiro3 += 1;
+                merge.inserir(f2.getInfo(ponteiro2));
                 ponteiro2 += 1;
-                c[ponteiro3] = f1.getFila[ponteiro1];
-                ponteiro3 += 1;
-                ponteiro1 += 1;
             }
         }
 
         while(ponteiro1 < f1.getTamanho()) {
-            c[ponteiro3] = f1.getFila[ponteiro1];
-            ponteiro3 += 1;
+            merge.inserir(f1.getInfo(ponteiro1));
             ponteiro1 += 1;
         }
 
         while(ponteiro2 < f2.getTamanho()) {
-            c[ponteiro3] = f2.getFila[ponteiro2];
-            ponteiro3 += 1;
+            merge.inserir(f2.getInfo(ponteiro2));
             ponteiro2 += 1;
         }
-        return c;
+        return merge;
+    }
+
+    public Fila mergeNos(Fila f1, Fila f2) {
+        Fila merge = new Fila();
+
+        Dados no1Atual = f1.getPrimeiroValor();
+        Dados no2Atual = f2.getPrimeiroValor();
+
+        while(no1Atual != null && no2Atual != null) {
+            if (no1Atual.getValor() < no2Atual.getValor()) {
+                merge.inserir(no1Atual.getValor());
+                no1Atual = no1Atual.getProximo();
+            } else {
+                merge.inserir(no2Atual.getValor());
+                no2Atual = no2Atual.getProximo();
+            }
+        }
+
+        while(no1Atual != null) {
+            merge.inserir(no1Atual.getValor());
+            no1Atual = no1Atual.getProximo();
+        }
+        while(no2Atual != null) {
+            merge.inserir(no2Atual.getValor());
+            no2Atual = no2Atual.getProximo();
+        }
+        return merge;
     }
 
 }
