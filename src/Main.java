f4f1507fb6f9638f26d881466a09d210098d3726
GeandroMarques <marquess.2608@gmail.com>
2025-09-07 14:29:28 -0300

    Atualização final merge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Fila f1 = new Fila();
+        f1.inserir(2);
+        f1.inserir(5);
+        f1.inserir(10);
+        f1.inserir(211);
+
+        Fila f2 = new Fila();
+        f2.inserir(3);
+        f2.inserir(4);
+
+
+        Merge merge = new Merge();
+        Fila filamerge = merge.mergeNos(f1, f2);
+        System.out.println(filamerge.imprimir());
 
     }
 }
--- src/Merge.java
+++ src/Merge.java
@@ -5,57 +5,27 @@ public class Merge {
     public Merge() {
     }
 
-    public Fila mergePonteiro(Fila f1, Fila f2) {
-        Fila merge = new Fila();
-
-        ponteiro1 = 0;
-        ponteiro2 = 0;
-
-        while(ponteiro1 < f1.getTamanho() && ponteiro2 < f2.getTamanho()) {
-            if(f1.getInfo(ponteiro1) < f2.getInfo(ponteiro2)) {
-                merge.inserir(f1.getInfo(ponteiro1));
-                ponteiro1 += 1;
-            } else {
-                merge.inserir(f2.getInfo(ponteiro2));
-                ponteiro2 += 1;
-            }
-        }
-
-        while(ponteiro1 < f1.getTamanho()) {
-            merge.inserir(f1.getInfo(ponteiro1));
-            ponteiro1 += 1;
-        }
-
-        while(ponteiro2 < f2.getTamanho()) {
-            merge.inserir(f2.getInfo(ponteiro2));
-            ponteiro2 += 1;
-        }
-        return merge;
-    }
-
     public Fila mergeNos(Fila f1, Fila f2) {
         Fila merge = new Fila();
 
-        Dados no1Atual = f1.getPrimeiroValor();
-        Dados no2Atual = f2.getPrimeiroValor();
-
-        while(no1Atual != null && no2Atual != null) {
-            if (no1Atual.getValor() < no2Atual.getValor()) {
-                merge.inserir(no1Atual.getValor());
-                no1Atual = no1Atual.getProximo();
+        while (!f1.vazia() && !f2.vazia()) {
+            if (f1.getPrimeiroValor() < f2.getPrimeiroValor()) {
+                merge.inserir(f1.getPrimeiroValor());
+                f1.remover();
             } else {
-                merge.inserir(no2Atual.getValor());
-                no2Atual = no2Atual.getProximo();
+                merge.inserir(f2.getPrimeiroValor());
+                f2.remover();
             }
         }
 
-        while(no1Atual != null) {
-            merge.inserir(no1Atual.getValor());
-            no1Atual = no1Atual.getProximo();
+        while (!f1.vazia()) {
+            merge.inserir(f1.getPrimeiroValor());
+            f1.remover();
         }
-        while(no2Atual != null) {
-            merge.inserir(no2Atual.getValor());
-            no2Atual = no2Atual.getProximo();
+
+        while (!f2.vazia()) {
+            merge.inserir(f2.getPrimeiroValor());
+            f2.remover();
         }
         return merge;
     }
