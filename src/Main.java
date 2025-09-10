9b4bf0f8a5f40d548bb64f1d71383a258be11a6c
Juliano Teles Tolomini Biasotto <julianobiasotto@gmail.com>
2025-09-10 18:51:19 -0300

    Main interativo pro usuário

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,88 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Fila f1 = new Fila();
-        f1.inserir(2);
-        f1.inserir(5);
-        f1.inserir(10);
-        f1.inserir(211);
+        Scanner scanner = new Scanner(System.in);
 
-        Fila f2 = new Fila();
-        f2.inserir(3);
-        f2.inserir(4);
-
-
+        Fila fila1 = new Fila();
+        Fila fila2 = new Fila();
+        int numAnterior1 = -1_000_000_000;
+        int numAnterior2 = -1_000_000_000;
+        int opcao = 0;
         Merge merge = new Merge();
-        Fila filamerge = merge.mergeNos(f1, f2);
-        System.out.println(filamerge.imprimir());
 
+        while(opcao != 11){
+            System.out.println("***************************");
+            System.out.println("1 - Inserir número na Pilha");
+            System.out.println("2 - Remover número da Pilha");
+            System.out.println("3 - Imprimir Pilha");
+            System.out.println("4 - Inserir número na Fila 1");
+            System.out.println("5 - Remover número da Fila 1");
+            System.out.println("6 - Imprimir Fial 1");
+            System.out.println("7 - Inserir número na Fila 2");
+            System.out.println("8 - Remover número da Fila 2");
+            System.out.println("9 - Imprimir Fila 2");
+            System.out.println("10 - Merge Fila 1 e Fila 2");
+            System.out.println("11 - Fechar Programa");
+            System.out.println("***************************");
+            System.out.print("Digite a sua escolha: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            int numero;
+            switch (opcao){
+                case 1:
+                    System.out.print("Digite um número inteiro: ");
+                    numero = scanner.nextInt();
+                    scanner.nextLine();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    do {
+                        System.out.print("Digite um número inteiro (maior que:" + numAnterior1 + "): ");
+                        numero = scanner.nextInt();
+                    }while (numAnterior1 >= numero);
+                    numAnterior1 = numero;
+                    fila1.inserir(numero);
+                    scanner.nextLine();
+                    break;
+                case 5:
+                    System.out.println(fila1.remover());
+                    if(fila1.vazia()) numAnterior1 = -1_000_000_000;
+                    break;
+                case 6:
+                    System.out.println("Fila 1: " + fila1.imprimir());
+                    break;
+                case 7:
+                    do {
+                        System.out.print("Digite um número inteiro (maior que:" + numAnterior2 + "): ");
+                        numero = scanner.nextInt();
+                    }while (numAnterior2 >= numero);
+                    numAnterior2 = numero;
+                    fila2.inserir(numero);
+                    scanner.nextLine();
+                    break;
+                case 8:
+                    System.out.println(fila2.remover());
+                    if(fila2.vazia()) numAnterior2 = -1_000_000_000;
+                    break;
+                case 9:
+                    System.out.println("Fila 2: " + fila2.imprimir());
+                    break;
+                case 10:
+                    System.out.println("Fila 1: " + fila1.imprimir());
+                    System.out.println("Fila 2: " + fila2.imprimir());
+                    System.out.println("Merge da Fila 1 + Fila 2: " +merge.mergeNos(fila1, fila2).imprimir());
+                    break;
+                default: break;
+            }
+
+            System.out.print("Pressione qualquer botão para continuar");
+            scanner.nextLine();
+        }
+
+        scanner.close();
     }
 }
