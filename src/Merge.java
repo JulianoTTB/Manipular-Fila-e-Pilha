7e9e4d6c4b98c70a18060eaaeb6e9ecded04d615
GeandroMarques <marquess.2608@gmail.com>
2025-09-11 18:16:04 -0300

    Atualização na classe merge com o metodo transformarFileEmVetor e mergeVetorial

--- src/Merge.java
+++ src/Merge.java
@@ -30,4 +30,63 @@ public class Merge {
         return merge;
     }
 
+    public static int[] tranformarFilaEmVetor(Fila f1) {
+        int tamanhoVetor = 0;
+        Fila f1Copia = new Fila();
+
+        while(!f1.vazia()) {
+            int no = f1.remover();
+            f1Copia.inserir(no);
+            tamanhoVetor += 1;
+        }
+
+        int [] f1Vetor = new int[tamanhoVetor];
+        int i = 0;
+
+        while(!f1Copia.vazia()) {
+            int no = f1Copia.remover();
+            f1Vetor[i++] = no;
+            f1.inserir(no);
+        }
+
+        return f1Vetor;
+    }
+
+    public static int [] mergeVetorial(Fila f1, Fila f2) {
+        int [] f1Vetor = tranformarFilaEmVetor(f1);
+        int [] f2Vetor = tranformarFilaEmVetor(f2);
+
+        int [] mergeVetor = new int[f1.getTamanho() + f2.getTamanho()];
+
+        int ponteiro1 = 0;
+        int ponteiro2 = 0;
+        int ponteiro3 = 0;
+        while(ponteiro1 < f1.getTamanho() && ponteiro2 < f2.getTamanho()) {
+            if (f1Vetor[ponteiro1] < f2Vetor[ponteiro2]) {
+                mergeVetor[ponteiro3] = f1Vetor[ponteiro1];
+                ponteiro3 += 1;
+                ponteiro1 += 1;
+            } else {
+                mergeVetor[ponteiro3] = f2Vetor[ponteiro2];
+                ponteiro3 += 1;
+                ponteiro2 += 1;
+            }
+        }
+
+        while (ponteiro1 < f1.getTamanho()) {
+            mergeVetor[ponteiro3] = f1Vetor[ponteiro1];
+            ponteiro3 += 1;
+            ponteiro1 += 1;
+        }
+
+        while (ponteiro2 < f2.getTamanho()) {
+            mergeVetor[ponteiro3] = f2Vetor[ponteiro2];
+            ponteiro3 += 1;
+            ponteiro2 += 1;
+        }
+
+        return mergeVetor;
+    }
+
+
 }
